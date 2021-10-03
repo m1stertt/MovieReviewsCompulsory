f187d23e11c2daa724e424e89e57c54aec2ed15e
Thomas Truelsen <52206366+m1stertt@users.noreply.github.com>
2021-10-03 16:53:06 +0200

    More setup..

--- MovieReviewsCompulsory.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MovieReviewsCompulsory.Domain
-{
-    public class Class1
-    {
-    }
-}
--- MovieReviewsCompulsory.Domain/Service/ReviewService.cs
+++ MovieReviewsCompulsory.Domain/Service/ReviewService.cs
@@ -1,7 +1,72 @@
-﻿namespace MovieReviewsCompulsory.Domain
+﻿using System.Collections.Generic;
+using MovieReviewsCompulsory.Core.IServices;
+using MovieReviewsCompulsory.Domain.IRepositories;
+
+namespace MovieReviewsCompulsory.Domain.Service
 {
-    public class ReviewService
+    public class ReviewService : IReviewService
     {
         
+        private readonly IRepository _repository;
+
+        public ReviewService(IRepository repository)
+        {
+            _repository = repository;
+        }
+        
+        public int GetNumberOfReviewsFromReviewer(int reviewer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double GetAverageRateFromReviewer(int reviewer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetNumberOfRatesByReviewer(int reviewer, int rate)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetNumberOfReviews(int movie)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double GetAverageRateOfMovie(int movie)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetNumberOfRates(int movie, int rate)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<int> GetMoviesWithHighestNumberOfTopRates()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<int> GetMostProductiveReviewers()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<int> GetTopRatedMovies(int amount)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<int> GetTopMoviesByReviewer(int reviewer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<int> GetReviewersByMovie(int movie)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
