f337a102b357ce113b469097291c33f7b4df056a
Snoozy <snoozy1995@gmail.com>
2021-10-03 21:55:00 +0200

    Tests initial push

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,18 +1,78 @@
 using System;
+using System.Collections.Generic;
 using Moq;
 using MovieReviewsCompulsory.Core.IServices;
+using MovieReviewsCompulsory.Core.Models;
+using MovieReviewsCompulsory.Domain.IRepositories;
+using MovieReviewsCompulsory.Domain.Service;
 using Xunit;
 
 namespace Tests
 {
     public class Tests
     {
-        public Mock<IReviewService> mock = new Mock<IReviewService>();
-        
-        [Fact]
-        public void Test1()
+        private Mock<IRepository> _mock;
+
+        public Tests()
         {
-            Assert.True(true);
+            var data = new List<Review>();
+            data.Add(new Review
+            {
+                Reviewer = 1,
+                Grade = 2,
+                Movie = 1,
+                Date = DateTime.Now
+            });
+            data.Add(new Review
+            {
+                Reviewer = 1,
+                Grade = 4,
+                Movie = 2,
+                Date = DateTime.Now
+            });
+            data.Add(new Review
+            {
+                Reviewer = 2,
+                Grade = 5,
+                Movie = 1,
+                Date = DateTime.Now
+            });
+            data.Add(new Review
+            {
+                Reviewer = 3,
+                Grade = 3,
+                Movie = 2,
+                Date = DateTime.Now
+            });
+            data.Add(new Review
+            {
+                Reviewer = 4,
+                Grade = 5,
+                Movie = 3,
+                Date = DateTime.Now
+            });
+            _mock = new Mock<IRepository>();
+            _mock.Setup(x => x.ReadAll()).Returns(data);
+        }
+
+        [Theory]
+        [InlineData(1,2)]
+        [InlineData(2,1)]
+        [InlineData(3,1)]
+        [InlineData(4,1)]
+        public void GetNumberOfReviewsFromReviewer(int reviewer, int expected)
+        {
+            Assert.Equal(expected,new ReviewService(_mock.Object).GetNumberOfReviewsFromReviewer(reviewer));
+        }
+
+        [Theory]
+        [InlineData(1,3)]
+        [InlineData(2,5)]
+        [InlineData(3,3)]
+        [InlineData(4,5)]
+        public void GetAverageRateFromReviewer(int reviewer, double expected)
+        {
+            Assert.Equal(expected,new ReviewService(_mock.Object).GetAverageRateFromReviewer(reviewer));
         }
     }
 }
